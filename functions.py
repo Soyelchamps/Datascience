801eaf40880b7eb22bf1c385db47d1f66e981f7f
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2023-02-13 21:56:09 -0600

    functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+def myFunction(param1, param2):
+    return param1 + param2
+
+
+def myFuncion2():
+    print("Inside myFuncion2")
+    return
+
+
+print(myFunction(2, 4))
+print(type(myFuncion2()))
