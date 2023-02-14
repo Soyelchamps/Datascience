ef5803c08f08f426a80d07d3c7706ef4bb028e2d
Carlos Diaz <92885752+Soyelchamps@users.noreply.github.com>
2023-02-13 21:40:10 -0600

    Case

---
+++ conditionals.py
@@ -0,0 +1,7 @@
+number = int(input("Ingresa un numero: "))
+if ((number > 5 and number < 10) or (number > 15 and number < 20)):
+    print("True")
+elif (number > 50):
+    print("number > 50 ")
+else:
+    print("else")
